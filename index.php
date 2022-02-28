00c747d464dc4c015dedac1f65e53b147376dc8f
killy147 <81945963+killy147@users.noreply.github.com>
2022-03-01 06:45:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Wonderfull Bogor</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="main">
+            <img src="bogor.png" alt="">
+            <ul>
+                <li><a href="index.php" class="active">Beranda</a></li>
+                <li><a href="pemesanan.php">Wisata Bogor</a></li>
+                <li><a href="harga.php">Daftar Harga</a></li>
+                <li><a href="pemesanan.php">Pesan Tiket</a></li>
+            </ul>
+        </div>
+        <div class="title">
+            <h1>WISATA KOTA BOGOR</h1>
+            <h2>"Wisata yang sangat indah dan sangat cocok untuk dikunjungi di hari libur”</h2>
+        </div>
+		<div class="footer">
+			Copyright @ 2022 Ibnu Aqil
+		</div>
+        </div>
+</header>
+</body>
+
+</html>
